c6cd17787265b6469145bda97ba87e9f847f2f8f
Skyggebjerg <ole@skyggebjerg.dk>
2024-10-04 13:18:27 +0200

    working loop

--- src/main.cpp
+++ src/main.cpp
@@ -25,43 +25,45 @@ Unit扩展多个I2C设备，并依次扫描从设备的I2C地址。
 #define X_LOCAL  100
 #define Y_LOCAL  35
 #define X_OFFSET 160
-#define Y_OFFSET 34
+#define Y_OFFSET 15
 
 #define PaHub_I2C_ADDRESS 0x70
 
 ClosedCube::Wired::TCA9548A tca9548a;
 
 void setup() {
-    M5.begin();
-    M5.Power.begin();
-    tca9548a.address(PaHub_I2C_ADDRESS);  // Set the I2C address.  设置I2C地址
-    M5.Lcd.setTextFont(4);
-    M5.Lcd.setCursor(70, 0, 4);
-    M5.Lcd.setTextColor(YELLOW, TFT_BLACK);
-    M5.Lcd.println(("PaHUB Example"));
-    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    Wire.begin(2,1);
+    auto cfg = M5.config();
+    AtomS3.begin(cfg);
+
+    tca9548a.address(PaHub_I2C_ADDRESS);  // Set the I2C address.
+    AtomS3.Display.setTextColor(GREEN);
+    AtomS3.Display.setTextSize(2);
+    AtomS3.Display.drawString("PaHUB Example", AtomS3.Display.width() / 2, AtomS3.Display.height() / 2);
+
 }
 
 void loop() {
     uint8_t returnCode = 0;
     uint8_t address;
     for (uint8_t channel = 0; channel < TCA9548A_MAX_CHANNELS; channel++) {
-        M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * channel, FRONT);
-        M5.Lcd.printf(
-            "                                                                ");
-        M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * channel, FRONT);
-        M5.Lcd.printf("CH%d : ", channel);
+        
+        AtomS3.Display.clear();
+        AtomS3.Display.drawString("C: ", 5, 5);
+        AtomS3.Display.drawString(String(channel), 40, 5 + Y_OFFSET * channel);
+
         returnCode = tca9548a.selectChannel(channel);
         if (returnCode == 0) {
             for (address = 0x01; address < 0x7F; address++) {
                 Wire.beginTransmission(address);
                 returnCode = Wire.endTransmission();
                 if (returnCode == 0) {
-                    Serial.print("I2C device = ");
-                    M5.Lcd.printf("0X%X  ", address);
+                    if (address != 112) {
+                    AtomS3.Display.drawString(String(address), 75, 5 + Y_OFFSET * channel);
+                    }
                 }
             }
         }
-        delay(200);
+        delay(1000);
     }
 }
