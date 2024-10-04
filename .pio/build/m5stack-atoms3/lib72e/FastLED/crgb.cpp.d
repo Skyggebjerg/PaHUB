54d44c2ab93a560e5e9d5f96dcce2684a2e545c0
Skyggebjerg <ole@skyggebjerg.dk>
2024-10-04 10:34:39 +0200

    init

---
+++ .pio/build/m5stack-atoms3/lib72e/FastLED/crgb.cpp.d
@@ -0,0 +1,8 @@
+.pio/build/m5stack-atoms3/lib72e/FastLED/crgb.cpp.o: \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/crgb.cpp \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/crgb.h \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/chsv.h \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/namespace.h \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/color.h \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/lib8tion/types.h \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/force_inline.h
---
+++ .pio/build/m5stack-atoms3/lib72e/FastLED/platforms/esp/32/led_strip/led_strip_rmt_dev_idf4.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/m5stack-atoms3/lib72e/FastLED/platforms/esp/32/led_strip/led_strip_rmt_dev_idf4.cpp.o: \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/platforms/esp/32/led_strip/led_strip_rmt_dev_idf4.cpp \
+ .pio/libdeps/m5stack-atoms3/FastLED/src/platforms/esp/32/led_strip/enabled.h \
+ C:/Users/ole/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_idf_version.h
